48032a78b01f0b06e4b94dc4aa5d07d73a0e8b21
sroman520 <roman.sa@husky.neu.edu>
2020-04-05 11:13:39 -0400

    treemap attempted with chart function

--- js/treemap.js
+++ js/treemap.js
@@ -1,8 +1,20 @@
+// initialize treemap
 function treemap() {
+
+    // CODE FROM: https://www.d3-graph-gallery.com/graph/treemap_json.html
+
     // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    let margin = {
+        top: 10,
+        right: 10,
+        bottom: 10,
+        left: 10
+    },
         width = 445 - margin.left - margin.right,
-        height = 445 - margin.top - margin.bottom;
+        height = 445 - margin.top - margin.bottom,
+        ourBrush = null,
+        selectableElements = d3.select(null),
+        dispatcher;
 
     // append the svg object to the body of the page
     // var svg = d3.select("#vis-holder")
@@ -13,51 +25,51 @@ function treemap() {
     //     .attr("transform",
     //         "translate(" + margin.left + "," + margin.top + ")");
 
-    let svg = d3.select(selector)
-      .append("svg")
-        .attr("preserveAspectRatio", "xMidYMid meet")
-        .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
-        .classed("svg-content", true);
+    function chart(selector, data) {
+        let svg = d3.select(selector)
+            .append("svg")
+            .attr("preserveAspectRatio", "xMidYMid meet")
+            .attr("viewBox", [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+            .classed("svg-content", true);
+        svg = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        // read json data
+        d3.json("data/vendors.csv", function (data) {
 
-    // read json data
-    d3.json("data/vendors.csv", function (data) {
+            // Give the data to this cluster layout:
+            var root = d3.hierarchy(data).sum(function (d) { return d.value }) // Here the size of each leave is given in the 'value' field in input data
 
-        // Give the data to this cluster layout:
-        var root = d3.hierarchy(data).sum(function (d) { return d.value }) // Here the size of each leave is given in the 'value' field in input data
+            // Then d3.treemap computes the position of each element of the hierarchy
+            d3.treemap()
+                .size([width, height])
+                .padding(2)
+                (root)
 
-        // Then d3.treemap computes the position of each element of the hierarchy
-        d3.treemap()
-            .size([width, height])
-            .padding(2)
-            (root)
+            // use this information to add rectangles:
+            svg
+                .selectAll("rect")
+                .data(root.leaves())
+                .enter()
+                .append("rect")
+                .attr('x', function (d) { return d.x0; })
+                .attr('y', function (d) { return d.y0; })
+                .attr('width', function (d) { return d.x1 - d.x0; })
+                .attr('height', function (d) { return d.y1 - d.y0; })
+                .style("stroke", "black")
+                .style("fill", "slateblue")
 
-        // use this information to add rectangles:
-        svg
-            .selectAll("rect")
-            .data(root.leaves())
-            .enter()
-            .append("rect")
-            .attr('x', function (d) { return d.x0; })
-            .attr('y', function (d) { return d.y0; })
-            .attr('width', function (d) { return d.x1 - d.x0; })
-            .attr('height', function (d) { return d.y1 - d.y0; })
-            .style("stroke", "black")
-            .style("fill", "slateblue")
-
-        // and to add the text labels
-        svg
-            .selectAll("text")
-            .data(root.leaves())
-            .enter()
-            .append("text")
-            .attr("x", function (d) { return d.x0 + 5 })    // +10 to adjust position (more right)
-            .attr("y", function (d) { return d.y0 + 20 })    // +20 to adjust position (lower)
-            .text(function (d) { return d.data.name })
-            .attr("font-size", "15px")
-            .attr("fill", "white")
-    })
-
+            // and to add the text labels
+            svg
+                .selectAll("text")
+                .data(root.leaves())
+                .enter()
+                .append("text")
+                .attr("x", function (d) { return d.x0 + 5 })    // +10 to adjust position (more right)
+                .attr("y", function (d) { return d.y0 + 20 })    // +20 to adjust position (lower)
+                .text(function (d) { return d.data.name })
+                .attr("font-size", "15px")
+                .attr("fill", "white")
+        })
+    }
 }
